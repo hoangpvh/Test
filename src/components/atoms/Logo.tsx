061db5df4817535293e1888347fc07a4de720adc
Hoang <pvhoangk45@gmai.com>
2024-11-11 17:38:20 +0700

    Header UI

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,11 +1,9 @@
 import Image from 'next/image';
 
-import logo from '@/public/Logo.svg';
-
 const Logo: React.FC = () => {
   return (
     <div className="w-10 h-10 relative">
-      <Image src={logo} alt="Logo" width={40} height={40} />
+      <Image src="/Logo.png" alt="Logo" width={40} height={40} />
     </div>
   );
 };
