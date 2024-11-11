2e9c519331fb533d6bc198941150767eb066e27c
Hoang <pvhoangk45@gmai.com>
2024-11-11 17:04:26 +0700

    Add font Helvetica

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
         seravek: ['Seravek', 'sans-serif'],
+        sans: ['Helvetica', 'Arial', 'sans-serif'], 
       },
     },
   },
