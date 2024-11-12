6b14e89ed97eec6f5aa2280339e5efad65e85780
Hoang <pvhoangk45@gmai.com>
2024-11-12 23:55:08 +0700

    Fix file Banner use file data.ts

--- src/components/organisms/banner/Banner.tsx
+++ src/components/organisms/banner/Banner.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { platformsList } from '@/assets/data';
 import ContentSection from '@/components/molecules/banner/ContentSection';
 import EmailInputGroup from '@/components/molecules/banner/EmailInputGroup';
 import PlatformCard from '@/components/molecules/banner/PlatformCard';
@@ -10,29 +11,13 @@ const Banner: React.FC = () => {
       <ContentSection />
       <EmailInputGroup />
 
-      <PlatformCard
-        className="w-[1448px] h-[120px] mt-[-143px] flex justify-between items-center"
-        platforms={[
-          { iconName: 'Notion Logomark', altText: 'Notion Logo' },
-          { iconName: 'Webflow Logomark', altText: 'Webflow Logo' },
-        ]}
-      />
-
-      <PlatformCard
-        className="w-[1056px] h-[120px] mt-[36px] flex justify-between items-center"
-        platforms={[
-          { iconName: 'Shopify Logomark', altText: 'Shopify Logo' },
-          { iconName: 'Wordpress Logomark', altText: 'Wordpress Logo' },
-        ]}
-      />
-
-      <PlatformCard
-        className="w-[636px] h-[120px] mt-[-13px] flex justify-between items-center"
-        platforms={[
-          { iconName: 'Visual Studio Code', altText: 'Visual Code Logo' },
-          { iconName: 'Figma Logomark', altText: 'Figma Logo' },
-        ]}
-      />
+      {platformsList.map((platform, index) => (
+        <PlatformCard
+          key={index}
+          className={platform.className}
+          platforms={platform.platforms}
+        />
+      ))}
     </div>
   );
 };
