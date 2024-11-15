75c1206735e762589201e17b6ffc42f3dc8e74ce
Hoang <pvhoangk45@gmai.com>
2024-11-15 17:48:17 +0700

    Change logo from .png to .svg

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -5,13 +5,13 @@ const Logo: React.FC = () => {
     <div className="relative">
       {/* Logo cho desktop */}
       <div className="hidden lg:block w-10 h-10 relative">
-        <Image src="/Logo.png" alt="Desktop Logo" width={40} height={40} />
+        <Image src="/Logo.svg" alt="Desktop Logo" width={40} height={40} />
       </div>
 
       {/* Logo cho mobile */}
       <div className="block lg:hidden w-7 h-7 relative">
         <Image
-          src="/Mobile Logo.png"
+          src="/Mobile Logo.svg"
           alt="Mobile Logo"
           width={24}
           height={24}
