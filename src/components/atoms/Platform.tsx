3d937c332fe4007053ddd0cf803418052a6da3fb
Hoang <pvhoangk45@gmai.com>
2024-11-15 16:36:59 +0700

    Change img from .png to .svg

--- src/components/atoms/Platform.tsx
+++ src/components/atoms/Platform.tsx
@@ -13,7 +13,7 @@ const Platform: React.FC<PlatformProps> = ({
     <div className="w-10 h-10 p-1.5 lg:w-[120px] lg:h-[120px] lg:p-2 bg-white rounded lg:rounded-3xl  border border-[#ebeff6] flex flex-col justify-center items-center gap-2.5">
       <div className="w-7 h-7 lg:w-[60px] lg:h-[60px] relative">
         <Image
-          src={`/${iconName}.png`}
+          src={`/${iconName}.svg`}
           alt={altText}
           width={60}
           height={60}
