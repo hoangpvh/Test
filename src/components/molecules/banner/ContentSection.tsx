e242ba219f3f2959231f2f948490a5eb8f53b03c
Hoang <pvhoangk45@gmai.com>
2024-11-15 17:20:02 +0700

    Fix color gradient

--- src/components/molecules/banner/ContentSection.tsx
+++ src/components/molecules/banner/ContentSection.tsx
@@ -2,7 +2,13 @@ const ContentSection: React.FC = () => {
   return (
     <div className="w-full h-[209px] 2xl:h-[206px] flex flex-col justify-start items-center lg:gap-10 gap-5">
       <div className="w-full mx-[20px] h-28 flex justify-center items-center">
-        <div className="absolute w-[216px] lg:w-[435px] h-12 lg:h-28 bg-gradient-to-r from-[#d2d5ff] border-l-4 border-[#565add] 2xl:ml-[228px] self-end ml-[-120px] lg:mb-[-4px] mb-1 -z-100 md:ml-[-320px] md:-mb-1 lg:ml-[300px]" />
+        <div
+          className="absolute w-[216px] lg:w-[435px] h-14 lg:h-28 border-l-4 border-[#565add] 2xl:ml-[228px] self-end ml-[-120px] lg:mb-[-4px] mb-1 -z-100 md:ml-[-320px] md:-mb-1 lg:ml-[300px]"
+          style={{
+            background:
+              'linear-gradient(to right, rgba(210, 213, 255, 1), rgba(240, 241, 255, 0))',
+          }}
+        />
         <div className="absolute text-center text-[#2b1c50] text-4xl 2xl:text-[90px] md:text-[60px] font-bold font-['Helvetica'] md:leading-[72px] leading-[60px] w-full flex flex-col justify-center lg:flex-row">
           <span>Code with Joy.</span>
           <span>&nbsp;Innovate with Fun!</span>
