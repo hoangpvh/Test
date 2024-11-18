ef413fd991431d47d1b77dea96c6274869affccf
Pham Van Hoang <pvhoangk45@gmail.com>
2024-11-18 17:39:43 +0700

    Fix structure follow atomic design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Banner from '@/components/organisms/banner/Banner'
 import Header from '@/components/organisms/header/Header'
 
-function MyApp() {
+function HomePage() {
   return (
-    <div className="w-full h-[5676px] flex flex-col items-center justify-start">
+    <div className="w-full flex flex-col items-center justify-start">
       <Header />
       <Banner />
     </div>
   )
 }
 
-export default MyApp
+export default HomePage
