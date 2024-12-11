47437b66ce1a7bdf45ac04030c22b89b0121b9f5
Pham Van Hoang <pvhoangk45@gmail.com>
2024-12-11 15:15:34 +0700

    Remove debug

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,16 +2,12 @@ import { createNavigation } from 'next-intl/navigation'
 import { defineRouting } from 'next-intl/routing'
 
 export const routing = defineRouting({
-  // A list of all locales that are supported
   locales: ['en', 'ja'],
 
-  // Used when no locale matches
   defaultLocale: 'ja',
   localeDetection: true,
 })
 
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
 export type Locale = (typeof routing.locales)[number]
 export const { Link, redirect, usePathname, useRouter } =
   createNavigation(routing)
